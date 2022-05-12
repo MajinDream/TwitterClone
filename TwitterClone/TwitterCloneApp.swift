ed051954b6e6d177c2330aeb8a75a13520cf3a5a
Dias Manap <diasmanap1@gmail.com>
2022-05-12 15:54:02 +0600

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Dias Manap on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
